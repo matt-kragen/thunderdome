b80c098ba11f30992ac7054c19dee7d3eb8f65e1
Matt Kragen <56685055+matt-kragen@users.noreply.github.com>
2023-01-07 21:43:19 -0800

    Solve challenge

---
+++ pangram.py
@@ -0,0 +1,21 @@
+# Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+
+# pangrams has the following parameter(s):
+
+# string s: a string to test
+# Returns
+
+# string: either 'pangram' or 'not pangram'
+
+def pangrams(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for letter in alphabet:
+        if letter not in s.lower():
+            return 'not pangram'
+    return 'pangram'
+
+
+print(pangrams('the quick brown fox jumps over the lazy dog'))
+print(pangrams('the quick brown fox jumps over the lazy do'))
+print(pangrams('We promptly judged antique ivory buckles for the next prize'))
+print(pangrams('12345'))
